3c12da167eeb9a058d8510bd0798a6289825b873
Juan David Tapia Frias <juandavidtapia123@gmail.com>
2026-03-24 20:31:36 -0600

    listo

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,15 @@
-import { Sequelize } from 'sequelize-typescript'
+import { Sequelize } from 'sequelize-typescript'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const db = new Sequelize(process.env.DB_URL! , {
-      models: [__dirname + '/../models/**/*.ts']
+const db = new Sequelize(process.env.DB_URL!, {
+  models: [__dirname + '/../models/**/*.ts'],
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
 
 export default db
